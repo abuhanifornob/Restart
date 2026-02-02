7e58bce2fb5edabb3038a941809558e2120a8b7c
Abu Hanif <hanifcse90@gmail.com>
2026-02-02 12:22:12 +0600

    Map Function

--- const-arry.js
+++ const-arry.js
@@ -1,2 +1,21 @@
-const box=['hello','bangla']
-console.log(box)
+// const box=['hello','bangla']
+// console.log(box)
+// let userList=['hanif','kalam','slalm']
+// userList.push('santo')
+
+// console.log(userList)
+// userList.pop()
+// console.log(userList)
+
+
+// Arry Build in Function
+//................Map...............
+
+const userInfo=[
+    {name:"Abu Hanif",email:"hanif90@gmail.com"},
+    {name:"Salam",email:"salam90@gmail.com"}
+]
+const userList=userInfo.map(user=>{
+    return user.email
+})
+console.log(userList)
