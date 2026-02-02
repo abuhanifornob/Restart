8306f3e3a1e6d905593e2f8820c4029e9daa70f0
Abu Hanif <hanifcse90@gmail.com>
2026-02-02 14:27:05 +0600

    Default Paramete Explor

---
+++ default-parameter.js
@@ -0,0 +1,21 @@
+
+function add(num1,num2=0){
+    const total=num1+num2;
+    console.log(total)
+}
+
+add(10)
+
+function gestName(name="Gest"){
+    console.log(`Hello ${name}`)
+}
+
+gestName("Baker")
+/*
+add,subtruct=0
+multiplecation=1
+array=[]
+object={}
+
+
+*/
